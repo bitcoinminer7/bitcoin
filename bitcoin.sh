28e1966556cbb4842c25f8d40c9e1c99c1799125
bitcoinminer7 <61884599+bitcoinminer7@users.noreply.github.com>
2020-03-06 21:02:56 +0300

    Create bitcoin.sh

---
+++ bitcoin.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+wget https://bitcoin.org/bin/bitcoin-core-0.19.0.1/bitcoin-0.19.0.1-x86_64-linux-gnu.tar.gz
+tar xf bitcoin-0.19.0.1-x86_64-linux-gnu.tar.gz
+cd bitcoin-0.19.0.1
+cp -r * /
+bitcoin-cli getnewaddress "" legacy
+bitcoin-cli backupwallet backup.dat
+cat backup.dat
